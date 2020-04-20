9d0ecd5326bb3ea814dc6881c9bec554e56d11b6
DHL <475003925@qq.com>
2020-04-20 20:19:43 +0800

    111

--- App/modeles/user/movie_user_modele.py
+++ App/modeles/user/movie_user_modele.py
@@ -1,29 +1,39 @@
-from flask_restful import fields
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from App.ext import db
 from App.modeles import BaseModel
 from App.modeles.user.model_constant import PERMISSION_NONE
 
-
-
-
-class MovieUser(BaseModel):
-
-    username = db.Column(db.String(32),unique=True)
-    _password = db.Column(db.String(256))
+class User(BaseModel):
+    __tablename__ = "user"
+    userId = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    userName = db.Column(db.String(128),unique=True)
+    userPassword = db.Column(db.String(1024))
+    addressCode = db.Column(db.String(256))
+    addressDefaultName = db.Column(db.String(256))
+    userPhoto = db.Column(db.String(128))
+    userIntroduction = db.Column(db.String(1024))
+    isBindingPhone = db.Column(db.Integer)
     phone = db.Column(db.String(32),unique=True)
-    is_delete = db.Column(db.Boolean,default=False)
-    permission = db.Column(db.Integer,default=PERMISSION_NONE)
-
+    isBindingWx = db.Column(db.Integer)
+    wxId = db.Column(db.String(128))
+    isVip = db.Column(db.Integer)
+    memberFlag = db.Column(db.Integer)
+    memberLevel = db.Column(db.String(10))
+    memberStatus = db.Column(db.String(16))
+    money = db.Column(db.FLOAT)
+    myBalanceIcon = db.Column(db.String(128))
+    myBalanceTitle = db.Column(db.String(128))
 
     @property
-    def password(self):
-        raise Exception("password con't access")
+    def _userPassword(self):
+        raise Exception("password not access")
 
-    @password.setter
-    def password(self,password_value):
-        self._password = generate_password_hash(password_value)
+    @_userPassword.setter
+    def _userPassword(self,value):
+        self.userPassword = generate_password_hash(value)
 
-    def check_password(self,password_value):
-        return check_password_hash(self._password,password_value)
+    def check_passwprd(self,password):
+        return check_password_hash(self.userPassword,password)
+
